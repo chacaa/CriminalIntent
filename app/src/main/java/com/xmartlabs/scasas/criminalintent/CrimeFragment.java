6aaf40aae1f5beb3c995868e375bb983fad4c43d
Chaca <scacas@xmartlabs.com>
2017-02-06 17:02:42 -0300

    changes suggested in the pull request

--- app/src/main/java/com/xmartlabs/scasas/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/xmartlabs/scasas/criminalintent/CrimeFragment.java
@@ -2,17 +2,17 @@ package com.xmartlabs.scasas.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnTextChanged;
 
 public class CrimeFragment extends Fragment {
-  @BindView(R.id.cime_title)
+  @BindView(R.id.crime_title)
   EditText titleField;
 
   private Crime crime = new Crime();
@@ -20,24 +20,12 @@ public class CrimeFragment extends Fragment {
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     View view = inflater.inflate(R.layout.fragment_crime, container, false);
-    setupViewListeners();
+    ButterKnife.bind(this, view);
     return view;
   }
 
-  private void setupViewListeners() {
-    titleField.addTextChangedListener(new TextWatcher() {
-      @Override
-      public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-      }
-
-      @Override
-      public void onTextChanged(CharSequence s, int start, int before, int count) {
-        crime.setTitle(s.toString());
-      }
-
-      @Override
-      public void afterTextChanged(Editable s) {
-      }
-    });
+  @OnTextChanged(R.id.crime_title)
+  void onTitleTextChange(CharSequence newTitle) {
+    crime.setTitle(newTitle.toString());
   }
 }
